a32480233cacb62e6421c151206751ca08f8fe63
liuxin <liuxin@izaodao.org>
2020-05-15 14:33:14 +0800

    Initial commit

---
+++ fat-dubbo-consumer/src/main/java/com/fat/learn/FatDubboConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.fat.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatDubboConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FatDubboConsumerApplication.class, args);
+    }
+
+}
---
+++ fat-dubbo-producer/src/main/java/com/fat/learn/FatDubboProducerApplication.java
@@ -0,0 +1,13 @@
+package com.fat.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatDubboProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FatDubboProducerApplication.class, args);
+    }
+
+}
