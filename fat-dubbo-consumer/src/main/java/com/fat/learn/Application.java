5b204f02cd257440d3985a9fa52566a416308836
liuxin <liuxin@izaodao.org>
2020-05-27 15:21:19 +0800

    nacos集群 配置

--- fat-dubbo-consumer/src/main/java/com/fat/learn/Application.java
+++ fat-dubbo-consumer/src/main/java/com/fat/learn/Application.java
@@ -3,6 +3,7 @@ package com.fat.learn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @ImportResource(locations = {"classpath:dubbo-consumer.xml"})
@@ -12,4 +13,13 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    /**
+     * 启动 string.
+     *
+     * @return the string
+     */
+    @GetMapping("/index")
+    public String index() {
+        return "fat dubbo消费者微服务启动完成！";
+    }
 }
--- fat-dubbo-producer/src/main/java/com/fat/learn/Application.java
+++ fat-dubbo-producer/src/main/java/com/fat/learn/Application.java
@@ -3,6 +3,7 @@ package com.fat.learn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @ImportResource(locations = {"classpath:dubbo-provider.xml"})
@@ -10,6 +11,16 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    /**
+     * 启动 string.
+     *
+     * @return the string
+     */
+    @GetMapping("/index")
+    public String index() {
+        return "fat dubbo生产者微服务启动完成！";
     }
 
 }
